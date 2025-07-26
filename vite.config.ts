1b0784b842591723e2c6001ad84893f29a027eb6
Ahmed Yasin Koculu <koculu@gmail.com>
2025-07-27 00:00:29 +0200

    define test paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    include: ['packages/**/*.{test,spec}.{js,cjs,mjs,ts,cts,mts,jsx,tsx}'],
+  },
 })
