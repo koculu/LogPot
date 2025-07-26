08b4a3ec98078865a0faf709ee85662f947f141a
Ahmed Yasin Koculu <koculu@gmail.com>
2025-07-27 00:05:57 +0200

    skip doc-site

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths({
+      skip: (dir) => dir.includes('docs-site'),
+    }),
+  ],
   test: {
     include: ['packages/**/*.{test,spec}.{js,cjs,mjs,ts,cts,mts,jsx,tsx}'],
   },
